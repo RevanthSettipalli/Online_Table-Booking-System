1efd6cec7db1c2173529b45cca14ae4e06485844
Revanth Settipalli <revanthsettipallii@gmail.com>
2026-05-22 01:20:31 +0530

    Final booking system

--- js/app.js
+++ js/app.js
@@ -1,60 +1,63 @@
-function toggleAuth(){
+document
+.getElementById("authForm")
 
-    const title=document.getElementById("authTitle");
-    const text=document.getElementById("authToggleText");
-    
-    if(title.innerText==="Register"){
-    title.innerText="Login";
-    text.innerText="Don't have an account?";
-    }
-    else{
-    title.innerText="Register";
-    text.innerText="Already have an account?";
-    }
-    
-    }
-    
-    document
-    .getElementById("authForm")
-    .addEventListener("submit",(e)=>{
-    
-    e.preventDefault();
-    
-    document.getElementById("auth").classList.add("hidden");
-    
-    document
-    .getElementById("mainWebsite")
-    .classList.remove("hidden");
-    
-    });
-    
-    
-    
-    function showSection(id){
-    
-    ["home","booking","gallery","contact"]
-    .forEach(section=>{
-    
-    document
-    .getElementById(section)
-    .style.display="none";
-    
-    });
-    
-    document
-    .getElementById(id)
-    .style.display="block";
-    
-    }
-    
-    
-    
-    document
-    .getElementById("bookingForm")
-    .addEventListener("submit",(e)=>{
-    
-    e.preventDefault();
-    
-    alert("Table Reserved Successfully!");
-    
-    });
+.addEventListener(
+
+"submit",
+
+function(e){
+
+e.preventDefault();
+
+document
+.getElementById("auth")
+.style.display="none";
+
+document
+.getElementById("mainWebsite")
+.classList.remove("hidden");
+
+}
+
+);
+
+
+function showSection(id){
+
+document
+.getElementById("home")
+.style.display="none";
+
+document
+.getElementById("booking")
+.style.display="none";
+
+document
+.getElementById("confirmationPage")
+.style.display="none";
+
+document
+.getElementById(id)
+.style.display="block";
+
+}
+
+
+document
+.getElementById("bookingForm")
+
+.addEventListener(
+
+"submit",
+
+function(e){
+
+e.preventDefault();
+
+showSection(
+"confirmationPage"
+);
+
+}
+
+);
